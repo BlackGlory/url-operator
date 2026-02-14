6877e2ae4da380aa7c0453e733014c935e502e4c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-14 06:41:24 +0000

    fix(set-protocol): edge cases

--- __tests__/set-protocol.spec.ts
+++ __tests__/set-protocol.spec.ts
@@ -2,24 +2,48 @@ import { setProtocol } from '@src/set-protocol.js'
 
 describe('setProtocol', () => {
   test('protocol ends with `:`', () => {
-    const url = new URL('protocol://hostname')
+    const url = new URL('foo://bar')
 
     const result = setProtocol(url, 'http:')
 
     expect(result).not.toBe(url)
     expect(result.protocol).toBe('http:')
-    expect(result.href).toBe('http://hostname/')
-    expect(url.href).toBe('protocol://hostname')
+    expect(result.href).toBe('http://bar/')
+    expect(url.href).toBe('foo://bar')
   })
 
   test('protocol does not ends with `:`', () => {
-    const url = new URL('protocol://hostname')
+    const url = new URL('foo://bar')
 
     const result = setProtocol(url, 'http')
 
     expect(result).not.toBe(url)
     expect(result.protocol).toBe('http:')
-    expect(result.href).toBe('http://hostname/')
-    expect(url.href).toBe('protocol://hostname')
+    expect(result.href).toBe('http://bar/')
+    expect(url.href).toBe('foo://bar')
+  })
+
+  describe('edge: without `//`', () => {
+    test('to known protocol', () => {
+      const url = new URL('foo:bar')
+
+      const result = setProtocol(url, 'http')
+
+      expect(result).not.toBe(url)
+      expect(result.protocol).toBe('http:')
+      expect(result.href).toBe('http://bar/')
+      expect(url.href).toBe('foo:bar')
+    })
+
+    test('to unknown protocol', () => {
+      const url = new URL('foo:bar')
+
+      const result = setProtocol(url, 'baz')
+
+      expect(result).not.toBe(url)
+      expect(result.protocol).toBe('baz:')
+      expect(result.href).toBe('baz:bar')
+      expect(url.href).toBe('foo:bar')
+    })
   })
 })
--- src/set-protocol.ts
+++ src/set-protocol.ts
@@ -1,13 +1,21 @@
 export function setProtocol(url: URL, protocol: string): URL {
   const oldURL = new URL(url)
 
-  // WHATWG URL标准在更改协议时有一些怪癖, 故在此通过直接修改字符串来达到目的.
+  // WHATWG URL标准在更改协议时具有怪癖, 会阻止与[Special Scheme]协议相关的修改.
+  // 这意味着协议为`http:`的URL无法更改为`foo:`协议, 协议为`foo:`的URL也无法更改为`http:`协议.
+  // 为了绕过这项限制, 此处直接替换URL字符串.
+  // [Special Scheme]: https://url.spec.whatwg.org/#special-scheme
+
+  // 此外, 还存在一种URL的host为空的情况, 即`mailto:`、`javascript:`等协议,
+  // 这些URL在协议后直接跟pathname而不是host.
+  // 要正确处理相关URL的协议转换, 需要完成host与pathname之间的转换, 而这在URL对象上也无法实现(赋值会静默失败).
+
   const newURL = new URL(
     oldURL.href.replace(
-      /^\S+:\/\//
+      /^\S+:/
     , protocol.endsWith(':')
       ? protocol
-      : `${protocol}://`
+      : `${protocol}:`
     )
   )
 
