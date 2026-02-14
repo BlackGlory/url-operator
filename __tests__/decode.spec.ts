79afde5cf4fb4c45bb47a88d2a00b0cb5eb3eda2
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-14 08:31:54 +0000

    test(decode): add cases

--- __tests__/decode.spec.ts
+++ __tests__/decode.spec.ts
@@ -2,7 +2,7 @@ import { decode } from '@src/decode.js'
 
 describe('decode', () => {
   describe('absolute URLs', () => {
-    test('URL is not decoded', () => {
+    test('host', () => {
       const url = `protocol://${encodeURIComponent('你好世界')}`
 
       const result = decode(url)
@@ -10,17 +10,89 @@ describe('decode', () => {
       expect(result).toBe('protocol://你好世界')
     })
 
-    test('part of URL is already decoded', () => {
+    test('host (partial)', () => {
       const url = `protocol://你好${encodeURIComponent('世界')}`
 
       const result = decode(url)
 
       expect(result).toBe('protocol://你好世界')
     })
+
+    test('pathname', () => {
+      const url = `protocol://foo/${encodeURIComponent('你好世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('protocol://foo/你好世界')
+    })
+
+    test('pathname (partial)', () => {
+      const url = `protocol://foo/你好${encodeURIComponent('世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('protocol://foo/你好世界')
+    })
+
+    test('pathname (special characters)', () => {
+      const url = `protocol://foo/${encodeURIComponent('你好/世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe(`protocol://foo/你好${encodeURIComponent('/')}世界`)
+    })
+
+    test('querystring', () => {
+      const url = `protocol://foo/?${encodeURIComponent('你好')}=${encodeURIComponent('世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('protocol://foo/?你好=世界')
+    })
+
+    test('querystring (partial)', () => {
+      const url = `protocol://foo/?你${encodeURIComponent('好')}=世${encodeURIComponent('界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('protocol://foo/?你好=世界')
+    })
+
+    test('querystring (special characters)', () => {
+      const url = `protocol://foo/?${encodeURIComponent('你好')}=${encodeURIComponent('世/界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe(`protocol://foo/?你好=世${encodeURIComponent('/')}界`)
+    })
+
+    test('hash', () => {
+      const url = `protocol://foo/#${encodeURIComponent('你好世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('protocol://foo/#你好世界')
+    })
+
+    test('hash (partial)', () => {
+      const url = `protocol://foo/#你好${encodeURIComponent('世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('protocol://foo/#你好世界')
+    })
+
+    test('hash (special characters)', () => {
+      const url = `protocol://foo/#${encodeURIComponent('你好/世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe(`protocol://foo/#你好${encodeURIComponent('/')}世界`)
+    })
   })
 
   describe('relative URLs', () => {
-    test('URL is not decoded', () => {
+    test('pathname', () => {
       const url = `../${encodeURIComponent('你好世界')}`
 
       const result = decode(url)
@@ -28,12 +100,68 @@ describe('decode', () => {
       expect(result).toBe('../你好世界')
     })
 
-    test('part of URL is already decoded', () => {
+    test('pathname (partial)', () => {
       const url = `../你好${encodeURIComponent('世界')}`
 
       const result = decode(url)
 
       expect(result).toBe('../你好世界')
+    })
+
+    test('pathname (special characters)', () => {
+      const url = `../${encodeURIComponent('你好/世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe(`../你好${encodeURIComponent('/')}世界`)
+    })
+
+    test('querystring', () => {
+      const url = `../?${encodeURIComponent('你好')}=${encodeURIComponent('世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('../?你好=世界')
+    })
+
+    test('querystring (partial)', () => {
+      const url = `../?你${encodeURIComponent('好')}=世${encodeURIComponent('界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('../?你好=世界')
+    })
+
+    test('querystring (special characters)', () => {
+      const url = `../?${encodeURIComponent('你好')}=${encodeURIComponent('世/界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe(`../?你好=世${encodeURIComponent('/')}界`)
+    })
+
+    test('hash', () => {
+      const url = `../#${encodeURIComponent('你好世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('../#你好世界')
+    })
+
+    test('hash (partial)', () => {
+      const url = `../#你好${encodeURIComponent('世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe('../#你好世界')
+    })
+
+    test('hash (special characters)', () => {
+      const url = `../#${encodeURIComponent('你好/世界')}`
+
+      const result = decode(url)
+
+      expect(result).toBe(`../#你好${encodeURIComponent('/')}世界`)
     })
   })
 })
--- src/decode.ts
+++ src/decode.ts
@@ -1,3 +1,4 @@
+// 该函数仅仅是`decodeURI`的别名, 因为`decodeURI`通过了所有测试用例.
 export function decode(url: string): string {
   return decodeURI(url)
 }
