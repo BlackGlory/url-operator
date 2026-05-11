aad9e2edd94bd9faed9c7263163d4de56423ea5c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-05-11 15:11:13 +0000

    refactor(append-pathname): improve performance

--- src/append-pathname.ts
+++ src/append-pathname.ts
@@ -1,11 +1,10 @@
 export function appendPathname(url: URL, pathname: string): URL {
-  const baseURL = new URL(url)
-  if (!baseURL.pathname.endsWith('/')) baseURL.pathname += '/'
+  const newURL = new URL(url)
+  if (!newURL.pathname.endsWith('/')) newURL.pathname += '/'
 
-  const newURL = new URL(pathname.replace(/^\/+/, ''), baseURL)
-  newURL.search = baseURL.search
-  newURL.hash = baseURL.hash
-  newURL.pathname = newURL.pathname.replace(/\/+$/, '')
+  const relativePathname = pathname.replace(/^\/+/, '')
+  const newPathname = new URL(relativePathname, newURL).pathname.replace(/\/+$/, '')
+  newURL.pathname = newPathname
 
   return newURL
 }
